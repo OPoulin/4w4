069a3b74aa97b6d8e9c73afcd8eaa89ee746c9c3
Olivier Poulin <456crdv@gmail.com>
2024-03-28 12:15:02 -0400

    Interface_mobile_plus_couleurs

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
         <header class="entete__menu">
             <label id="burger" for="chk_burger">
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
+                <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
             </label>
             <input id="chk_burger" type="checkbox">
             <?php wp_nav_menu(array("container"=>"nav")); ?>
