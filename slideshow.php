be806ec3a38d3b933c8dd5f5167e4af2d129881a
Olivier Poulin <456crdv@gmail.com>
2024-03-31 12:12:27 -0400

    debut_creation_slideshow_perso

---
+++ slideshow.php
@@ -0,0 +1,45 @@
+<div class="slideshow-container">
+
+  <div class="images__slideshow fondu">
+    <div class="numero__texte">1 / 5</div>
+    <img src="images/city_hall.png" style="width:100%">
+    <div class="texte">Découvrez de nombreuses nouvelles cultures</div>
+  </div>
+
+  <div class="images__slideshow fondu">
+    <div class="numero__texte">2 / 5</div>
+    <img src="images/Cruise.png" style="width:100%">
+    <div class="texte">Une de nos merveilleuses croisières</div>
+  </div>
+
+  <div class="images__slideshow fondu">
+    <div class="numero__texte">3 / 5</div>
+    <img src="images/Machu_Picchu.png" style="width:100%">
+    <div class="texte">L'iconique Machu Picchu</div>
+  </div>
+
+  <div class="images__slideshow fondu">
+    <div class="numero__texte">4 / 5</div>
+    <img url="images/Montagne.png" style="width:100%">
+    <div class="texte">Les magnifiques vues de nos randonnées</div>
+  </div>
+
+  <div class="images__slideshow fondu">
+    <div class="numero__texte">5 / 5</div>
+    <img src="images/plage.png" style="width:100%">
+    <div class="texte">Nos destinations tropicales pour tous vos besoins de détente</div>
+  </div>
+
+  <a class="precedent" onclick="rajouterImage(-1)">&#10094;</a>
+  <a class="suivant" onclick="rajouterImage(1)">&#10095;</a>
+</div>
+<br>
+
+
+<div style="text-align:center">
+  <span class="point" onclick="currentSlide(1)"></span>
+  <span class="point" onclick="currentSlide(2)"></span>
+  <span class="point" onclick="currentSlide(3)"></span>
+  <span class="point" onclick="currentSlide(4)"></span>
+  <span class="point" onclick="currentSlide(5)"></span>
+</div>
