f88dbad7357819c484486c2489409a0c1d20b597
Olivier Poulin <456crdv@gmail.com>
2024-03-31 13:06:36 -0400

    avancement slideshow

--- slideshow.php
+++ slideshow.php
@@ -1,32 +1,33 @@
 <div class="slideshow-container">
+    
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">1 / 5</div>
-    <img src="images/city_hall.png" style="width:100%">
+    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/city_hall.png" style="width:100%">
     <div class="texte">Découvrez de nombreuses nouvelles cultures</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">2 / 5</div>
-    <img src="images/Cruise.png" style="width:100%">
+    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Cruise.png" style="width:100%">
     <div class="texte">Une de nos merveilleuses croisières</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">3 / 5</div>
-    <img src="images/Machu_Picchu.png" style="width:100%">
+    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Machu_Picchu.png" style="width:100%">
     <div class="texte">L'iconique Machu Picchu</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">4 / 5</div>
-    <img url="images/Montagne.png" style="width:100%">
+    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Montagne.png" style="width:100%">
     <div class="texte">Les magnifiques vues de nos randonnées</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">5 / 5</div>
-    <img src="images/plage.png" style="width:100%">
+    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/plage.png" style="width:100%">
     <div class="texte">Nos destinations tropicales pour tous vos besoins de détente</div>
   </div>
 
@@ -37,9 +38,11 @@
 
 
 <div style="text-align:center">
-  <span class="point" onclick="currentSlide(1)"></span>
-  <span class="point" onclick="currentSlide(2)"></span>
-  <span class="point" onclick="currentSlide(3)"></span>
-  <span class="point" onclick="currentSlide(4)"></span>
-  <span class="point" onclick="currentSlide(5)"></span>
+  <span class="point" onclick="imageActuelle(1)"></span>
+  <span class="point" onclick="imageActuelle(2)"></span>
+  <span class="point" onclick="imageActuelle(3)"></span>
+  <span class="point" onclick="imageActuelle(4)"></span>
+  <span class="point" onclick="imageActuelle(5)"></span>
 </div>
+
+<script></script>
