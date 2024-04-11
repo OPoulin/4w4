f28f5c2aace71ec00cc488f84548cf4cc16047cf
Olivier Poulin <456crdv@gmail.com>
2024-04-11 14:25:07 -0400

    fonction images sans serveur

--- slideshow.php
+++ slideshow.php
@@ -3,31 +3,31 @@
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">1 / 5</div>
-    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/city_hall.png" style="width:100%">
+    <img src="https://gftnth00.mywhc.ca/tim45/wp-content/uploads/2024/04/city_hall.png" style="width:100%">
     <div class="texte">Découvrez de nombreuses nouvelles cultures</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">2 / 5</div>
-    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Cruise.png" style="width:100%">
+    <img src="https://gftnth00.mywhc.ca/tim45/wp-content/uploads/2024/04/Cruise.png" style="width:100%">
     <div class="texte">Une de nos merveilleuses croisières</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">3 / 5</div>
-    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Machu_Picchu.png" style="width:100%">
+    <img src="https://gftnth00.mywhc.ca/tim45/wp-content/uploads/2024/04/Machu_Picchu.png" style="width:100%">
     <div class="texte">L'iconique Machu Picchu</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">4 / 5</div>
-    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/Montagne.png" style="width:100%">
+    <img src="https://gftnth00.mywhc.ca/tim45/wp-content/uploads/2024/04/Montagne.png" style="width:100%">
     <div class="texte">Les magnifiques vues de nos randonnées</div>
   </div>
 
   <div class="images__slideshow fondu">
     <div class="numero__texte">5 / 5</div>
-    <img src="http://localhost/4w4-op/wp-content/uploads/2024/03/plage.png" style="width:100%">
+    <img src="https://gftnth00.mywhc.ca/tim45/wp-content/uploads/2024/04/plage.png" style="width:100%">
     <div class="texte">Nos destinations tropicales pour tous vos besoins de détente</div>
   </div>
 
